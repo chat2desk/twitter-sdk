b7b8858256c65db286ec4740420f0644dfbfb41b
denis <avenger_den@mail.ru>
2018-05-23 16:07:59 +0300

    Check to send json add logs

--- lib/twitter_api_client/oauth.ex
+++ lib/twitter_api_client/oauth.ex
@@ -40,7 +40,7 @@ defmodule TwitterApiClient.OAuth do
     send_httpc_request(:get, request, options)
   end
 
-  def oauth_post(url, params, consumer_key, consumer_secret, access_token, access_token_secret, options) do
+  def oauth_post_n(url, params, consumer_key, consumer_secret, access_token, access_token_secret, options) do
     credentials = OAuther.credentials(
       consumer_key: consumer_key,
       consumer_secret: consumer_secret,
@@ -54,25 +54,24 @@ defmodule TwitterApiClient.OAuth do
     {header, req_params} = OAuther.header(params)
     Logger.info "START header - #{inspect header}"
     Logger.info "START req_params - #{inspect req_params}"
-
-    1/0
-    signed_params = get_signed_params(
-      "post", url, Poison.encode!(params), consumer_key, consumer_secret, access_token, access_token_secret)
-    Logger.info "SIGNED PARAMS - #{inspect signed_params}"
-    Logger.info "oauth_post url - #{inspect url}"
-#    Logger.info "oauth_post signed_params - #{inspect Enum.into(signed_params, %{})}"
-    Logger.info "oauth_post params - #{inspect params}"
-    headers = ["Content-Type": "application/json", "Authorization": URI.encode_query(signed_params)]
-    Logger.info "oauth_post headers - #{inspect headers}"
-    Logger.info "oauth_post encode params - #{inspect Poison.encode!(params)}"
     HTTPoison.post(url, Poison.encode!(params), headers)
+#    signed_params = get_signed_params(
+#      "post", url, Poison.encode!(params), consumer_key, consumer_secret, access_token, access_token_secret)
+#    Logger.info "SIGNED PARAMS - #{inspect signed_params}"
+#    Logger.info "oauth_post url - #{inspect url}"
+##    Logger.info "oauth_post signed_params - #{inspect Enum.into(signed_params, %{})}"
+#    Logger.info "oauth_post params - #{inspect params}"
+#    headers = ["Content-Type": "application/json", "Authorization": URI.encode_query(signed_params)]
+#    Logger.info "oauth_post headers - #{inspect headers}"
+#    Logger.info "oauth_post encode params - #{inspect Poison.encode!(params)}"
+#    HTTPoison.post(url, Poison.encode!(params), headers)
 #    request = {to_charlist(url), [], "application/json", Poison.encode!(Map.merge(Enum.into(signed_params, %{}), params))}
 #    request = {to_charlist(url), [{"Authorization", URI.encode_query(signed_params)}], 'application/json', Poison.encode!(params)}
 #    Logger.info "oauth_post request - #{inspect request}"
 #    send_httpc_request(:post, request, options)
   end
 
-  def oauth_post_old(url, params, consumer_key, consumer_secret, access_token, access_token_secret, options) do
+  def oauth_post(url, params, consumer_key, consumer_secret, access_token, access_token_secret, options) do
     Logger.info "POST CHECK params #{inspect params}"
     Logger.info "POST CHECK url #{inspect url}"
     signed_params = get_signed_params(
